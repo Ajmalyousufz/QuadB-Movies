d6ffe4a8f9814f45a916d3d6101c43e9c2750d08
Ajmalyousufza <ajmalyousufza@gmail.com>
2022-01-20 01:36:54 +0530

    Splash screen added and some animation added on splash screen

--- app/src/main/java/com/ajmalyousufza/quadbmovies/SplashScreen.java
+++ app/src/main/java/com/ajmalyousufza/quadbmovies/SplashScreen.java
@@ -2,13 +2,44 @@ package com.ajmalyousufza.quadbmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView splashImage;
+    TextView quadblogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        splashImage = findViewById(R.id.splshimg);
+        quadblogo = findViewById(R.id.quadblogo);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.zoom);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setDuration(2000);
+        //splashImage.startAnimation(animation);
+        quadblogo.startAnimation(animation);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
