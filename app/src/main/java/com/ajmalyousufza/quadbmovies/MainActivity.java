b77cf17a86a26823a87158596f5be91569fa2e87
Ajmalyousufza <ajmalyousufza@gmail.com>
2022-01-19 18:48:06 +0530

    Home and Search bottom  Navigation created with currusponding Fragments

--- app/src/main/java/com/ajmalyousufza/quadbmovies/MainActivity.java
+++ app/src/main/java/com/ajmalyousufza/quadbmovies/MainActivity.java
@@ -1,14 +1,47 @@
 package com.ajmalyousufza.quadbmovies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        getSupportFragmentManager().beginTransaction().replace(R.id.action_bar_container,new HomeFragment()).commit();
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                int id = item.getItemId();
+
+                switch (id){
+
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.search:
+                        fragment = new SearchFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.action_bar_container,fragment).commit();
+
+                return true;
+            }
+        });
     }
 }
